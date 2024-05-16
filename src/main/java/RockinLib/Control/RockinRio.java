90f825217a9a6b1f8f2a61a4aa6c25accc0e38ba
GalenSpooner <gspooner@cusd.me>
2024-05-16 12:32:46 -0700

    rockinlib additions

---
+++ src/main/java/RockinLib/Control/RockinRio.java
@@ -0,0 +1,7 @@
+package RockinLib.Control;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RockinRio extends SubsystemBase {
+
+}
--- src/main/java/RockinLib/PID/RockinSparkPIDSubsystem.java
+++ src/main/java/RockinLib/PID/RockinSparkPIDSubsystem.java
@@ -3,12 +3,14 @@ import java.time.chrono.HijrahChronology;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 
+import RockinLib.MotorControllers.RockinSparkMax;
 import RockinLib.MotorControllers.RockinTalon;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-public class RockinTalonPIDSubsystem extends SubsystemBase{
-    public RockinTalon[] talons;
+public class RockinSparkPIDSubsystem extends SubsystemBase{
+    public RockinSparkMax[] sparks;
     public PIDController pidController;
     public CANcoder encoder;
     private double pidOutput = 0;
@@ -23,23 +25,24 @@ public class RockinTalonPIDSubsystem extends SubsystemBase{
      * @param name
      
     **/
-    public RockinTalonPIDSubsystem(String name, PIDController pidController, double initStartPoint, CANcoder encoder, RockinTalon... talons){
-        this.talons = talons;
+    public RockinSparkPIDSubsystem(String name, PIDController pidController, double initStartPoint, CANcoder encoder, RockinSparkMax... sparks){
+        this.sparks = sparks;
         this.pidController = pidController;
         this.encoder = encoder;
         setpoint = initStartPoint;
         this.setName(name);
+        
     }
     /**
-     use this as a normal subsystem periodic
-      **/
+     * Use this instead of the normal periodic method
+     **/
     public void highLevelPeriodic(){
 
     }
     public void calcAndApply(double setpoint){
-        for(int x = 0; x <= talons.length; x++){
-         talons[x].set(pidController.calculate(encoder.getAbsolutePosition().getValueAsDouble(), setpoint));
-          SmartDashboard.putNumber((this.getName() + " motor " + (x+1) + " PID value"),pidController.calculate(encoder.getAbsolutePosition().getValueAsDouble(), setpoint));
+        for(int x = 0; x <= sparks.length; x++){
+         sparks[x].set(pidController.calculate(encoder.getAbsolutePosition().getValueAsDouble(), setpoint));
+         SmartDashboard.putNumber((this.getName() + " motor " + (x+1) + " PID value"),pidController.calculate(encoder.getAbsolutePosition().getValueAsDouble(), setpoint));
         }
     }
     public double getSetpoint(){
@@ -48,15 +51,16 @@ public class RockinTalonPIDSubsystem extends SubsystemBase{
     public void setSetpoint(double setpoint){
         this.setpoint = setpoint;
     }
-    @Override 
+   
     /** 
-     * Do not override this method. Instead use {@link RockinTalonPIDSubsystem#highLevelPeriodic()}
+     * Do not override this method. Instead use {@link RockinSparkPIDSubsystem#highLevelPeriodic()}
      *
     */
+     @Override 
     public void periodic(){
         calcAndApply(getSetpoint());
         highLevelPeriodic();
-        SmartDashboard.putNumber(this.getName() + " angle: ",encoder.getAbsolutePosition().getValueAsDouble());
+        
     }
 
 }
